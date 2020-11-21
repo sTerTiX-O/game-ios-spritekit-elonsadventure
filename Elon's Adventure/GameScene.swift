d6dcf53da5bde598052167a683fdc682533335c3
Malo Fonrose <malo.fonrose@gmail.com>
2020-11-21 15:31:54 +0100

    Le joueur se déplace en actionnant le joystick

--- Elon's Adventure/GameScene.swift
+++ Elon's Adventure/GameScene.swift
@@ -22,6 +22,11 @@ class GameScene: SKScene {
     //Measure
     var knobRadius : CGFloat = 50.0
     
+    // Sprite Engine
+    var previousTimeInterval : TimeInterval = 0
+    var playerIsFacingRight = true
+    let playerSpeed = 4.0
+
     //didmove
     override func didMove(to view: SKView) {
         player = childNode(withName: "player")
@@ -81,4 +86,23 @@ extension GameScene {
     }
 }
 
+// MARK: Game Loop
+extension GameScene {
+    
+    /// REMARQUE : Lors de la première version de cette méthode, le joueur disparaissait de l'écran instantanément.
+    ///   - C'était lié à un problème de knob qui n'était pas tout à fait positionné en 0,0
+    ///   - Du coup, comme lors du premier appel à update(), la valeur de 'previousTimeInterval' n'est pas initialisée, on calculait une valeur énorme de deltaTime et donc une valeur énorme de displacement qui faisait sortir le joueur de l'écran
+    ///   - En remettant le knob à (0,0) dans le fichier GameScene.sks, la valeur xPosition à 0 vient annuler l'abberation de la valeur de 'deltaTime' calculée
+    override func update(_ currentTime: TimeInterval) {
+        let deltaTime = currentTime - previousTimeInterval
+        previousTimeInterval = currentTime
 
+        // Player movement
+        guard let joystickKnob = joystickKnob else { return }
+        let xPosition = Double(joystickKnob.position.x)
+        let xDisplacement = ((deltaTime * playerSpeed) * xPosition)
+        let displacement = CGVector(dx: xDisplacement, dy: 0)
+        let move = SKAction.move(by: displacement, duration: 0)
+        player?.run(move)
+    }
+}
